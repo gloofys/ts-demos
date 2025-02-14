104e60227dcc800c0339d336c4e9d7e156b97856
fredb <fred.brosman06@gmail.com>
2025-02-14 08:37:33 +0200

    TodoList.tsx using useState and interface.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import {useState} from "react";
 import UserProfile from "./components/UserProfile.tsx";
+import TodoList from "./components/TodoList.tsx";
 
 const App = () => {
     const [counter, setCounter] = useState<number>(0);
@@ -9,6 +10,7 @@ const App = () => {
             <h2>{counter}</h2>
             <button onClick={() => setCounter(counter + 1)}>Counter</button>
             <UserProfile />
+            <TodoList/>
         </div>
     )
 }
